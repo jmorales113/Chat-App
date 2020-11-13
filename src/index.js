abc2404ab6300f4f102082ce9362b1350b60ecdf
jmorales113 <jmorales113@gmail.com>
2020-11-12 17:55:57 -0800

    Established connection between client and server

--- src/index.js
+++ src/index.js
@@ -13,9 +13,19 @@ const publicDirectoryPath = path.join(__dirname, "../public")
 
 app.use(express.static(publicDirectoryPath))
 
-io.on("connection", () => {
+let count = 0
+
+io.on("connection", (socket) => {
     console.log("New WebSocket connection")
-})
+    
+    socket.emit("countUpdated", count)
+
+    socket.on("increment", () => {
+        count++
+        // socket.emit("countUpdated", count)
+        io.emit("countUpdated", count)
+    })
+}) 
 
 server.listen(port, () => {
     console.log(`Server is up on port ${port}!`)
