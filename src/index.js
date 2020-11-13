f55d43435590c273cbc7eaa1a37b533683afe4ab
jmorales113 <jmorales113@gmail.com>
2020-11-12 19:48:06 -0800

    Broadcasting events when users join and leave chat

--- src/index.js
+++ src/index.js
@@ -10,20 +10,21 @@ const io = socketio(server)
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, "../public")
 
-
 app.use(express.static(publicDirectoryPath))
-
-let count = 0
 
 io.on("connection", (socket) => {
     console.log("New WebSocket connection")
-    
-    socket.emit("countUpdated", count)
 
-    socket.on("increment", () => {
-        count++
-        // socket.emit("countUpdated", count)
-        io.emit("countUpdated", count)
+    const msg = "Welcome!"
+    socket.emit("message", msg)
+    socket.broadcast.emit("message", "A new user has joined!")
+    
+    socket.on("sendMessage", (message) => {
+        io.emit("message", message)
+    })
+
+    socket.on("disconnect", () => {
+        io.emit("message", "A user has left!")
     })
 }) 
 
