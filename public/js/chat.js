bc2e1c29c95b0c24784d79ff710dcfb9caade790
jmorales113 <jmorales113@gmail.com>
2020-11-14 13:51:24 -0800

    Sharing user location

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,6 +8,22 @@ document.querySelector("#form").addEventListener("submit", (e) => {
     e.preventDefault()
 
     const message = document.querySelector("#message").value
-    
+
     socket.emit("sendMessage", message)
+})
+
+document.querySelector("#location").addEventListener("click", () => {
+    if (!navigator.geolocation) {
+        return alert("Geolocation is not supported by your browser.")
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude
+        const long = position.coords.longitude
+
+        socket.emit("sendLocation", {
+            latitude: lat, 
+            longitude: long
+        })
+    })
 })
