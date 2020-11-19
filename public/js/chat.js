d02ada42cdc796634cf5ed8f8f2db5bb67090abd
jmorales113 <jmorales113@gmail.com>
2020-11-18 16:57:07 -0800

    Rendering messages to screen

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,44 @@
 const socket = io()
 
+// Elements
+const $messageForm = document.querySelector("#message-form")
+const $messageFormInput = document.querySelector("#message-input")
+const $messageFormButton = document.querySelector("#message-button")
+const $locationButton = document.querySelector("#location-button")
+const $messages = document.querySelector("#messages")
+
+const messageTemplate = document.querySelector("#message-template").innerHTML
+const locationMessageTemplate = document.querySelector("#location-message-template").innerHTML
+
 socket.on("message", (msg) => {
     console.log(msg)
+    const html = Mustache.render(messageTemplate, {
+        msg
+    })
+    $messages.insertAdjacentHTML("beforeend", html)
 })
 
-document.querySelector("#form").addEventListener("submit", (e) => {
+socket.on("locationMessage", (url) => {
+    console.log(url)
+    const html = Mustache.render(locationMessageTemplate, {
+        url
+    })
+    $messages.insertAdjacentHTML("beforeend", html)
+})
+
+$messageForm.addEventListener("submit", (e) => {
     e.preventDefault()
 
-    const message = document.querySelector("#message").value
+    $messageFormButton.setAttribute("disabled", "disabled")
+
+    const message = document.querySelector("#message-input").value
 
     socket.emit("sendMessage", message, (error) => {
+        
+        $messageFormButton.removeAttribute("disabled")
+        $messageFormInput.value = ""
+        $messageFormInput.focus()
+
         if (error) {
             return console.log(error)
         }
@@ -18,10 +47,12 @@ document.querySelector("#form").addEventListener("submit", (e) => {
     })
 })
 
-document.querySelector("#location").addEventListener("click", () => {
+$locationButton.addEventListener("click", () => {
     if (!navigator.geolocation) {
         return alert("Geolocation is not supported by your browser.")
     }
+
+    $locationButton.setAttribute("disabled", "disabled")
 
     navigator.geolocation.getCurrentPosition((position) => {
         const lat = position.coords.latitude
@@ -31,6 +62,7 @@ document.querySelector("#location").addEventListener("click", () => {
             latitude: lat, 
             longitude: long
         }, () => {
+            $locationButton.removeAttribute("disabled")
             console.log("location shared!")
         })
     })
