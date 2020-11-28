1d759a7a80afbf77b34496065a61813695090a5a
jmorales113 <jmorales113@gmail.com>
2020-11-27 18:32:32 -0800

    Tracking users joining and leaving

--- src/index.js
+++ src/index.js
@@ -17,14 +17,19 @@ app.use(express.static(publicDirectoryPath))
 io.on("connection", (socket) => {
     console.log("New WebSocket connection")
 
-    socket.on("join", ({ username, room }) => {
-        socket.join(room)
+    socket.on("join", ({ username, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, username, room })
 
+        if (error) {
+            return callback(error)
+        }
+        
+        socket.join(user.room)
+        
         socket.emit("message", generateMessage("Welcome!"))
-        socket.broadcast.to(room).emit("message", generateMessage(`${username} has joined!`))
+        socket.broadcast.to(user.room).emit("message", generateMessage(`${user.username} has joined!`))
 
-        // socket.emit, io.emit, socket.broadcast.emit
-        // io.to.emit, socket.broadcast.to.emit
+        callback()
     })
     
     socket.on("sendMessage", (message, callback) => {
@@ -38,7 +43,11 @@ io.on("connection", (socket) => {
     })
 
     socket.on("disconnect", () => {
-        io.emit("message", generateMessage("A user has left!"))
+        const user = removeUser(socket.id)
+        console.log(user)
+        if (user) {
+            io.to(user.room).emit("message", generateMessage(`${user.username} has left!`))
+        }
     })
 }) 
 
